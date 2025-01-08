29aae242bb3d69ba915eaf18904288ad058284da
vladimir <vladimir.945g@gmail.com>
2025-01-08 13:26:23 +0500

    init commit

---
+++ src/components/React/ReactComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const ReactComponent = () => {
+    return <div className={styles.test}>Привет из React!</div>;
+};
+
+export default ReactComponent;

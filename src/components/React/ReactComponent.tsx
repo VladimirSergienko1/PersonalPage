9124540506c4e607ee742abf282faaf2ef289b00
vladimir <vladimir.945g@gmail.com>
2025-01-10 13:46:26 +0500

    added config

--- src/components/React/ReactComponent.tsx
+++ src/components/React/ReactComponent.tsx
@@ -2,7 +2,95 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 const ReactComponent = () => {
-    return <div className={styles.test}>Привет из React!</div>;
+  return (
+    <div className={styles.test}>
+      <div>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+        <span>Hello World</span>
+      </div>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+      <span>Hello World</span>
+    </div>
+  );
 };
 
 export default ReactComponent;
